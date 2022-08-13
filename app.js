4d38e2f504b5dc2c3e957475ab5c3aadfd53d83c
leonelta <ketchakaren@gmail.com>
2022-08-14 00:11:29 +0200

    styling the new class "sticky" that was added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+    //for the navbar to appear on scroll
     $(window).scroll(function(){
         if(this.scrollY > 200){
             $('.navbar').addClass("sticky");
