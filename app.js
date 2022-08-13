eb6ab18a51be22f05b0b017e04a0f630433342af
leonelta <ketchakaren@gmail.com>
2022-08-14 01:16:12 +0200

    crating a new variable typed

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
 
-    //for the navbar to appear on scroll
+     //for navbar to appear on scroll
     $(window).scroll(function(){
         if(this.scrollY > 200){
             $('.navbar').addClass("sticky");
-        }
-        else{
+        }else{
             $('.navbar').removeClass("sticky");
         }
+    });
+
+    var typed = new Typed(".typing-1",{
+        strings:[
+            "network engineer", "Developer"
+        ],
+        typespeed: 100,
+        backsp
     });
 });
